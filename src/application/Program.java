5354910397bf640fca41f0651285daec1d097b46
LucasRocha <lucascaixeta98@gmail.com>
2023-10-17 16:54:34 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,11 +32,17 @@ public class Program {
 		}
 
 		System.out.println("\n=== TEST 4: Seller insert ===");
-		Seller newSeller = new Seller(
-				null, "Eduardo", "eduardo@gmail.com", 
+		Seller newSeller = new Seller(null, 
+				"Eduardo", "eduardo@gmail.com", 
 				new Date(), 4000.0, department);
 
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! Novo id = " + newSeller.getId());
+
+		System.out.println("\n=== TEST 5: Seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Karol Ta Doida");
+		sellerDao.update(seller);
+		System.out.println("Update Completado");
 	}
 }
